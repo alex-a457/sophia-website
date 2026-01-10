0ce38e2f45050450b120c259ea6fd52fdc6102f0
Alex Britto <mailtoalexbritto@gmail.com>
2026-01-10 18:55:48 +0530

    blog page

---
+++ app/(pages)/blog/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BlogPage() {
+    return (
+        <div className="max-w-7xl mx-auto text-center mt-[2.813rem] mb-[2.5rem] lg:px-[10px] px-[5px]">
+            Blog page
+        </div>
+    );
+}
