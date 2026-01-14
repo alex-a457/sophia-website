8ca13e394be0320de262b32e687b2d97cb6f2eba
Alex Britto <mailtoalexbritto@gmail.com>
2026-01-14 22:52:24 +0530

    cart page header and footer

--- app/(shop)/cart/page.tsx
+++ app/(shop)/cart/page.tsx
@@ -1,6 +1,16 @@
+'use client';
+
 // app/cart/page.tsx
 import CartLayout from "@/components/cart/CartLayout";
+import Footer from "@/components/Footer";
+import Header from "@/components/headerComponents/Header";
 
 export default function CartPage() {
-  return <CartLayout />;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <CartLayout />
+      <Footer />
+    </div>
+  );
 }
